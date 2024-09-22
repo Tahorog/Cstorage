1724d3a04bef8d648eee8554cba9168d19f064b8
patategeniale <146565416+patategeniale@users.noreply.github.com>
2024-09-22 14:11:53 +0200

    Update PrintLoop.c

--- PrintLoop.c
+++ PrintLoop.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int n;  // Variable pour stocker le nombre d'affichages
+    int n;  
 
-    // Boucle do...while pour garantir que le nombre est positif
     do {
         printf("Veuillez entrer un nombre positif d'affichages : ");
         scanf("%d", &n);
         if (n <= 0) {
             printf("Le nombre doit être positif. Veuillez réessayer.\n");
         }
-    } while (n <= 0);  // Continue tant que le nombre est inférieur ou égal à 0
+    } while (n <= 0);  
 
-    // Boucle for pour afficher la chaine le nombre de fois demandé
     for (int i = 1; i <= n; i++) {
         printf("Bienvenue en TP : indice %d\n", i);
     }
