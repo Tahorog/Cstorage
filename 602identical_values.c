9508fc08c5224ace1c6f1e1fb4879a76c2a26d52
Foxye-11 <111603167+Foxye-11@users.noreply.github.com>
2024-09-27 14:23:29 +0200

    Create 602identical_values.c

---
+++ 602identical_values.c
@@ -0,0 +1,14 @@
+//Exercice 602
+
+       int entier_1, entier_2, entier_3;
+       printf("Entrez trois entiers : \n");
+       scanf("%d %d %d", &entier_1, &entier_2, &entier_3);
+       if (entier_1 == entier_2 == entier_3) {
+           printf("Tous les entiers saisis sont identiques.\n");
+       }
+       else if ((entier_1 == entier_2 && entier_3 != entier_1) || (entier_3 == entier_1 && entier_2 != entier_1)) {
+           printf("Seuls deux des trois entiers sont identiques.\n");
+       }
+       else {
+           printf("Tous les entiers saisis sont differents.\n");
+       }
