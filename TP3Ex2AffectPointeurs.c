abb7af32327ca26b8d108694b6e0c1293e7f69e0
Foxye-11 <111603167+Foxye-11@users.noreply.github.com>
2024-10-02 14:54:28 +0200

    Create TP3Ex2AffectPointeurs.c

---
+++ TP3Ex2AffectPointeurs.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int main(void) {
+    
+    //Exercice 2
+
+    printf("\n");
+
+    int val1;
+    char val2, lettre;
+    int* pt1;
+    char* pt2, * pt3;
+
+    pt1 = &val1;
+    pt2 = &val2;
+    pt3 = &lettre;
+
+    int** ppt1;
+    char** ppt2;
+    char** ppt3;
+
+    ppt1 = &pt1;
+    ppt2 = &pt2;
+    ppt3 = &pt3;
+    
+
+    val1 = 10;
+    val2 = 'e';
+    lettre = 'z';
+
+    printf("val1 = %d\n", val1);
+    printf("val2 = %c\n", val2);
+    printf("lettre = %c\n", lettre);
+    printf("\n");
+
+    *pt1 = 11;
+    *pt2 = 'o';
+    *pt3 = 'c';
+
+    printf("val1 = %d\n", *pt1);
+    printf("val2 = %c\n", *pt2);
+    printf("lettre = %c\n", *pt3);
+    printf("\n");
+
+    **ppt1 = 33;
+    **ppt2 = 'g';
+    **ppt3 = 'p';
+
+    printf("val1 = %d\n", **ppt1);
+    printf("val2 = %c\n", **ppt2);
+    printf("lettre = %c\n", **ppt3);
+    printf("\n");
+
+    return 0;
+}
