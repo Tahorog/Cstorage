bc7ec75f5a801e47375415c666a1dcefc29e473b
Kroncks <161207379+Kroncks@users.noreply.github.com>
2024-10-13 15:34:19 +0200

    Create main.c in 803

---
+++ 803/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "foncts.h"
+
+int main(void) {
+    int h=0,m=0;
+
+    while(1) {
+        printf("%dh%d\n",h,m);
+        sleep(1);
+        m=updatem(m);
+        h=updateh(m,h);
+    }
+    return 0;
+}
