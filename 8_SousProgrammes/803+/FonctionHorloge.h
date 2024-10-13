55e82216bd525d448a91156a7a97079de7f5b8fe
Foxye-11 <111603167+Foxye-11@users.noreply.github.com>
2024-10-13 16:46:37 +0200

    Create FonctionHorloge.h

---
+++ 8_SousProgrammes/803+/FonctionHorloge.h
@@ -0,0 +1,8 @@
+#ifndef FONCTION_HORLOGE_H
+#define FONCTION_HORLOGE_H
+
+int horlogeminute(int tempsm);
+int horlogeheure(int tempsm, int tempsh);
+char horlogeDemijournee(int tempsh, char Demijournee);
+
+#endif
