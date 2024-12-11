6fa669eded676a012365902755750e3a78dfbc65
Kroncks <161207379+Kroncks@users.noreply.github.com>
2024-12-11 12:19:51 +0100

    Create foncts.h

---
+++ 10_Tableaux/1008/foncts.h
@@ -0,0 +1,16 @@
+//
+// Created by Picard on 03/11/2024.
+//
+
+#ifndef FONCTS_H
+#define FONCTS_H
+
+#define LIGNES 4
+#define COLONES 5
+
+void remplir(char tab[LIGNES][COLONES]);
+void maj(char tab[LIGNES][COLONES]);
+void show(char tab[LIGNES][COLONES]);
+void invert(char tab[LIGNES][COLONES]);
+
+#endif //FONCTS_H
