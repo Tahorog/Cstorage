abbbdb7421e971593392fe778255e72d1ac65a3b
Kroncks <161207379+Kroncks@users.noreply.github.com>
2024-12-11 12:17:41 +0100

    Create foncts.h

---
+++ 10_Tableaux/1003/foncts.h
@@ -0,0 +1,16 @@
+//
+// Created by Picard on 03/11/2024.
+//
+
+#ifndef FONCTS_H
+#define FONCTS_H
+
+#define TAILLE 10
+
+void remplir(float tab[TAILLE]);
+void show(float tab[TAILLE]);
+float getMoy(float tab[TAILLE]);
+float getMax(float tab[TAILLE]);
+float getMin(float tab[TAILLE]);
+
+#endif //FONCTS_H
