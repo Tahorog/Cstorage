a8459b1103b1bf5e5cce8b13d725f3e623ce207e
Kroncks <161207379+Kroncks@users.noreply.github.com>
2024-12-11 11:34:45 +0100

    Create 1501.c

---
+++ 15_AllocationDynamique/1501.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+char * truc() {
+    char phrase1[100];
+    char phrase2[100];
+    int longueur =0;
+    printf("Enter Phrase: ");
+    fgets(phrase1,100,stdin);
+    longueur = strlen(phrase1);
+    phrase1[longueur-1] = '\0';
+    printf("Enter Phrase: ");
+    fgets(phrase2,100,stdin);
+    longueur += strlen(phrase2);
+
+    char *retour = (char*)malloc(longueur * sizeof(char)+3);
+    strcpy(retour, phrase1);
+    strcat(retour, " ");
+    strcat(retour, phrase2);
+    return retour;
+}
+
+
+int main(void) {
+
+    char *phrase = truc();
+
+    printf("%s\n", phrase);
+
+    return 0;
+}
