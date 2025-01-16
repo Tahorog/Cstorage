d78e1628449cf4bf53c462d2d0efca44cf4019bb
Kroncks <161207379+Kroncks@users.noreply.github.com>
2025-01-16 19:44:13 +0100

    Update 1603.c

--- 16_Recursivite/1603.c
+++ 16_Recursivite/1603.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int taille(char mot[100], int n) {
+    if (mot[n] == '\0') {
+        return n;
+    }
+    n+=1;
+    return taille(mot, n);
+}
+
+int main(void) {
+    char mot[100];
+    printf("Entrez un mot :\n");
+    scanf("%s", mot);
+
+    printf("Longueur du mot : %d\n",taille(mot,0));
+    return 0;
+}
 
