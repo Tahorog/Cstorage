c39269fd38444ed58f6f263d24618ccb2aed7cac
Kroncks <161207379+Kroncks@users.noreply.github.com>
2025-01-16 19:43:22 +0100

    Update 1602.c

--- 16_Recursivite/1602.c
+++ 16_Recursivite/1602.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int somme(int taille,int tableau[taille], int n) {
+    if (taille == n) {
+        return 0;
+    }
+    return (somme(taille,tableau,n+1)+tableau[n]);
+}
+
+int main(void) {
+    int sum = 0;
+    int taille = 0;
+    printf("Taille du tableau ?\n");
+    scanf("%d", &taille);
+    printf("Remplir le tableau\n");
+    int * tableau = malloc(sizeof(int) * taille);
+    for (int i = 0; i < taille; i++) {
+        printf("-");
+        scanf("%d", &tableau[i]);
+    }
+    printf("Somme des elements : %d\n",somme(taille,tableau,0));
+    free(tableau);
+    return 0;
+}
