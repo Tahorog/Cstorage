866eb067a22cc17a16948212149c3296b68e15c8
Kroncks <161207379+Kroncks@users.noreply.github.com>
2025-01-17 23:20:42 +0100

    Update 1605.c

--- 16_Recursivite/1605.c
+++ 16_Recursivite/1605.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int puissance(int n, int p) {
+    if (p == 0) {
+        return 1;
+    }
+    p-=1;
+    return puissance(n,p)*n;
+}
+
+int main(void) {
+    int n =0 ,p=0;
+    printf("Entrez un nombre :\n");
+    scanf("%d", &n);
+    printf("Entrez une puissance :\n");
+    scanf("%d", &p);
+
+    printf("%d^%d = %d\n",n,p,puissance(n,p));
+    return 0;
+}
 
